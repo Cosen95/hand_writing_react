d43ad0bcef8780fe7878e1a2b3e0b595d01419a8
Cosen95 <1263215592@qq.com>
2023-04-14 17:18:40 +0800

    feat: jsx bundle

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,67 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElementType,
+	ElementType
+} from 'shared/ReactTypes';
+
+// ReactElement
+
+const ReactElement = function (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
+		key,
+		ref,
+		props,
+		__mark: 'cosen'
+	};
+
+	return element;
+};
+
+export const jsx = (type: ElementType, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		// 将多余参数作为children
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = jsx;
---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,51 @@
+import { getPackageJSON, resolvePkgPath, getBaseRollupPlugins } from './utils';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const { name, module } = getPackageJSON('react');
+// react包路径
+const pkgPath = resolvePkgPath(name);
+// react产物路径
+const pkgDistPath = resolvePkgPath(name, true);
+const basePlugins = getBaseRollupPlugins();
+
+export default [
+	// react
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'index.js',
+			format: 'umd'
+		},
+		plugins: [
+			...basePlugins,
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime.js',
+				format: 'umd'
+			},
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime.js',
+				format: 'umd'
+			}
+		],
+		plugins: basePlugins
+	}
+];
