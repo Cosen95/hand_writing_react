578273a4a7a9156f1eff23ef70f847ff674efdf9
Cosen95 <1263215592@qq.com>
2023-05-10 23:35:58 +0800

    feat: useState实现

--- demos/test-fc/main.tsx
+++ demos/test-fc/main.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const root = document.querySelector('#root');
 
 function HomeIndex() {
-	return (
-		<div>
-			<span>cosen writing react</span>
-		</div>
-	);
+	const [count, setCount] = useState(6);
+	return <div>{count}</div>;
 }
 
 ReactDOM.createRoot(root).render(<HomeIndex />);
